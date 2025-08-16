3b1044ed3435cf6064f91c027baed7c15a0e17c4
yousifalansari <y.alansari2006@gmail.com>
2025-08-16 13:02:53 +0300

    added card data constants with IDs, image sources, and alt text for the suit cards which is the theme of the game

---
+++ js/app.js
@@ -0,0 +1,19 @@
+/*-------------- Constants -------------*/
+const cardsData = [
+  {
+    id: "clubs-1",
+    imgSrc: "https://i.ibb.co/2Y0xFPpX/card-clubs-1.png",
+    alt: "Club suit card"
+  },
+  {
+    id: "hearts-7",
+    imgSrc: "https://i.ibb.co/zWXw42mX/card-hearts-7.png",
+    alt: "Heart suit card"
+  },
+  {
+    id: "spades-13",
+    imgSrc: "https://i.ibb.co/sv4pR05r/card-spades-13.png",
+    alt: "Spade suit card"
+  }
+];
+
